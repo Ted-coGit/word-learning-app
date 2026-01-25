03146203fab21845b7373a0775f17d945660e594
Ted-coGit <jinhokyoung83@gmail.com>
2026-01-25 19:35:51 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WordLearningApp from './word-learning-app.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WordLearningApp />
+  </React.StrictMode>,
+)
